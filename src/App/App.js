6687ba1f3943f7c3e013959b6eae0297646bbeef
WIN-92LM5B90MKJ\Dasha <da18121996@yandex.by>
2021-02-20 20:47:04 +0300

    add: added styled-components

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Hello = styled.div`
+  text-align: center;
+  `
+
+function App() {
+  return (
+    <Hello className="App">
+      Hello
+    </Hello>
+  );
+}
+
+export default App;
