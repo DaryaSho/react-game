66beda840c72a5ae359b8947dd4d7dcdcc1975aa
DaryaSho <da18121996@yandex.by>
2021-02-27 13:46:59 +0300

    add: added swap to row and column

--- src/model/sudoku.js
+++ src/model/sudoku.js
@@ -30,36 +30,55 @@ export class Sudoku {
     }
 
     swap(){
-        const count  = this.getRandomInt(20) + 10;
-        for (let index = 0; index < 3; index++) {
+        const count  = this.getRandomInt(30) + 10;
+        for (let index = 0; index < count; index++) {
             
             this.swapRows();
+            this.swapColumn();
         }
     }
 
-    swapRows(){
-        const index = this.getRandomInt(9);
+    findIndexToSwap(index){
         const indentSize = this.getRandomInt(2)+1;
-        let index_2 = index;
-        const square = this.body[index].square;
-        if(square*3 > index)index_2 = index_2-indentSize;
-        else if(square*3 < index)index_2 = index_2+indentSize;
-        else if(square*3 === index)index_2 = indentSize===0? index_2+1: index_2-1; 
-    
+        const square = this.body[index].square-1;
+
+        if(square*3+2=== index) return index - indentSize;
+        else if(square*3+1 === index )return  indentSize === 0 ? index + 1: index - 1;
+        else if(square*3 === index) return index + indentSize;
+        return index;
+    }
+
+    swapColumn(){
+        const index = this.getRandomInt(8);
+        const index_2 = this.findIndexToSwap(index);
         for (let x = 0; x < this.size; x++) {
-            const val = this.body[index*9+x].value;
-            this.body[index*9+x].value = this.body[index_2*9+x].value;
-            this.body[index_2*9+x].value = val;
+            const val = this.body[index+x*9].value;
+            this.body[index+x*9].value = this.body[index_2+x*9].value;
+            this.body[index_2+x*9].value = val;
             
         } 
     }
 
     
+    swapRows(){
+        const index = this.getRandomInt(8);
+        const index_2 = this.findIndexToSwap(index);
+
+        for (let x = 0; x < this.size; x++) {
+            const val = this.body[index*9+x].value;
+            this.body[index*9+x].value = this.body[index_2*9+x].value;
+            this.body[index_2*9+x].value = val;  
+        } 
+    }
+
+    
     createBase(){
+        let index = 0
         for (let x = 0; x < this.size; x++) {
             for (let y = 0; y < this.size; y++) {
                 const number = Math.floor((x*Math.sqrt(this.size) + x/Math.sqrt(this.size) + y) % this.size + 1);
-                this.body.push({x, y, value: number, square:  3 * Math.floor(x/3) + Math.floor(y/3) + 1, isConst: true}); 
+                this.body.push({x, y, value: number, square:  3 * Math.floor(x/3) + Math.floor(y/3) + 1, isConst: true, index}); 
+                index++;
             }    
         }
     }
